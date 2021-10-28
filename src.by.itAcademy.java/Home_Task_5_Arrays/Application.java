45fa0f2b3cd095f94942400702bb633f58edee0f
Energetik <antonrazhko@gmail.com>
2021-10-28 23:13:10 +0300

    fixed all bugs

--- src.by.itAcademy.java/Home_Task_5_Arrays/Application.java
+++ src.by.itAcademy.java/Home_Task_5_Arrays/Application.java
@@ -21,17 +21,16 @@ public class Application {
         valueAverage(array); // Метод для получения значений
 
         // Задание 3
-        int[] array3= getArray3();
+        int[] array3 = getArray();
         printArray(array3);
         addValue(array3);
 
         //Задание4 (метод находится ниже)
-
-        }
+    }
 
     public static int[] getArray() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите размер массива(Задание №2)");
+        System.out.println("Введите размер массива");
         int length = scanner.nextInt();
         int[] array = new int[length];
         System.out.println("Заполните массив");
@@ -39,59 +38,51 @@ public class Application {
             array[i] = scanner.nextInt();
         }
         return array;
-    } //Задание 2 (создание массива)
+    } //Задание 2 и 3 (создание массива)
+
     public static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]+" ");
+            System.out.print(array[i] + " ");
         }
     } // задание 2 и 3 (вывод массива в консоль)
-    public static void valueAverage(int [] array){
-        int sum=0;
-        for (int i=0; i<array.length;i++){
-            sum= sum+array[i];
-        } double average = (double) sum/ array.length;
 
-        for (int j=0;j< array.length;j++){
-            if (array[j]<average){
-                System.out.println("Значения меньше среднеарифмитического");
-                System.out.println(array[j]+" ");
+    public static void valueAverage(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        double average = (double) sum / array.length;
+        System.out.println("Значения меньше среднеарифмитического");
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] < average) {
+                System.out.println(array[j] + " ");
             }
         }
     } // задание 2( вывод значений ,меньше среднеарифметического)
-    public static int [] getArray3(){
-        System.out.println("Введите длинну массива (Задание №3)");
-        Scanner scanner = new Scanner(System.in);
-        int length = scanner.nextInt();
-        int[] array3 = new int[length];
-        System.out.println("Заполните массив");
-        for (int i = 0; i < array3.length - 1; i++) {
-            array3[i] = scanner.nextInt();
-        }return array3;
-    } //Задание 3 (создание массива,за искл. последнего элемента)
-    public static void addValue (int[] array3){
+
+    public static void addValue(int[] array) {
         System.out.println("Введите индекс");
         Scanner scanner = new Scanner(System.in);
         int index = scanner.nextInt();// переменная для масива
         int value = scanner.nextInt();
-        for (int i = 0; i < array3.length; i++) {
-            if (array3[i] == index){
-                for (int j = array3.length-1;j>index;j--){
-                    array3[j]=array3[j-1];}
-            }
+        for (int j = array.length - 1; j > index; j--) {
+            array[j] = array[j - 1];
         }
-        array3[index]=value;
-        for (int i = 0; i < array3.length; i++) {
-            System.out.print(array3[i]+" ");
+        array[index] = value;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
-} //Задание 3 (запись допю значения в массив)
-    public static int getMaxValue(int[][] numbers){
-        int max =numbers[0][0];
-        for(int i=0;i< numbers.length;i++){
-            for (int j=0;j< numbers.length;j++){
-                if (max<numbers[i][j] ){
-                    max=numbers[i][j];
+    } //Задание 3 (запись доп. значения в массив)
+
+    public static int getMaxValue(int[][] numbers) {
+        int max = numbers[0][0];
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                if (max < numbers[i][j]) {
+                    max = numbers[i][j];
                 }
             }
-        }return max;
+        }
+        return max;
     } //Задание 4 (нахождение мах значения двухмерного массива)
 }
