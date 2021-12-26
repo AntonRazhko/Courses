274be218e4e5b380d584c7b1848313e6d43ba56e
Energetik <antonrazhko@gmail.com>
2021-12-26 15:45:58 +0300

    Homework 17 completed

--- src.by.itAcademy.java/Home_Task_17_Multithreading_sinchronized/Application.java
+++ src.by.itAcademy.java/Home_Task_17_Multithreading_sinchronized/Application.java
@@ -2,9 +2,9 @@ package Home_Task_17_Multithreading_sinchronized;
 
 public class Application {
     public static void main(String[] args) {
-        Object object= new Object();
-        for (int i=0; i<10;i++){
-            MyThread thread= new MyThread(object);
+        Object object = new Object();
+        for (int i = 0; i < 10; i++) {
+            MyThread thread = new MyThread(object);
             thread.start();
         }
     }
